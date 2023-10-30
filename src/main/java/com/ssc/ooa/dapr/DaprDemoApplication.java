397aec0a76ea6e9f5d0cb54a9b0197df1c4bea3c
Anand Nadar <anand.nadar@me.com>
2023-10-30 11:24:26 -0400

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/ssc/ooa/dapr/DaprDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ssc.ooa.dapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaprDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssc/ooa/dapr/DaprDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssc.ooa.dapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaprDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
